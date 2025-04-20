d9cdb23396527be59c951170efe89bbc37b38a71
岡野 正明 <yakyou@users.noreply.github.com>
2025-04-20 11:59:14 +0900

    source_ip(hostname)の横に送信元国(国旗)を表示するようにした。

--- index.php
+++ index.php
@@ -14,15 +14,51 @@ function isMySource($ip) {
 	}
 	return $ret;
 }
+//国コード(ISO 3166-1)を国旗のemojiにします
+function code2flag($countryCode) {
+	$ret = '';
+	if (!empty($countryCode)) {
+		$countryCode = str_replace('A', '&#x1f1e6;', $countryCode);
+		$countryCode = str_replace('B', '&#x1f1e7;', $countryCode);
+		$countryCode = str_replace('C', '&#x1f1e8;', $countryCode);
+		$countryCode = str_replace('D', '&#x1f1e9;', $countryCode);
+		$countryCode = str_replace('E', '&#x1f1ea;', $countryCode);
+		$countryCode = str_replace('F', '&#x1f1eb;', $countryCode);
+		$countryCode = str_replace('G', '&#x1f1ec;', $countryCode);
+		$countryCode = str_replace('H', '&#x1f1ed;', $countryCode);
+		$countryCode = str_replace('I', '&#x1f1ee;', $countryCode);
+		$countryCode = str_replace('J', '&#x1f1ef;', $countryCode);
+		$countryCode = str_replace('K', '&#x1f1f0;', $countryCode);
+		$countryCode = str_replace('L', '&#x1f1f1;', $countryCode);
+		$countryCode = str_replace('M', '&#x1f1f2;', $countryCode);
+		$countryCode = str_replace('N', '&#x1f1f3;', $countryCode);
+		$countryCode = str_replace('O', '&#x1f1f4;', $countryCode);
+		$countryCode = str_replace('P', '&#x1f1f5;', $countryCode);
+		$countryCode = str_replace('Q', '&#x1f1f6;', $countryCode);
+		$countryCode = str_replace('R', '&#x1f1f7;', $countryCode);
+		$countryCode = str_replace('S', '&#x1f1f8;', $countryCode);
+		$countryCode = str_replace('T', '&#x1f1f9;', $countryCode);
+		$countryCode = str_replace('U', '&#x1f1fa;', $countryCode);
+		$countryCode = str_replace('V', '&#x1f1fb;', $countryCode);
+		$countryCode = str_replace('W', '&#x1f1fc;', $countryCode);
+		$countryCode = str_replace('X', '&#x1f1fd;', $countryCode);
+		$countryCode = str_replace('Y', '&#x1f1fe;', $countryCode);
+		$countryCode = str_replace('Z', '&#x1f1ff;', $countryCode);
+	}
+	return $countryCode;
+}
 $reports = $db->query('
 	SELECT * 
 	FROM reports 
 	INNER JOIN report_records 
 	ON reports.report_metadata_org_name = report_records.report_metadata_org_name 
 	AND reports.report_metadata_report_id = report_records.report_metadata_report_id
+	LEFT OUTER JOIN ipinfos 
+	ON report_records.row_source_ip = ipinfos.ip 
 	ORDER BY reports.report_metadata_date_range_begin DESC
 	, reports.report_metadata_org_name
 	, reports.report_metadata_report_id
+	, ipinfos.country
 	, report_records.row_source_ip
 	, report_records.num
 ');
@@ -30,6 +66,7 @@ $reports = $db->query('
 <html>
 <head>
 <meta charset="utf-8">
+<script src="https://cdn.jsdelivr.net/npm/@twemoji/api@latest/dist/twemoji.min.js" crossorigin="anonymous"></script>
 <title>DMARC Reports</title>
 <style>
 	body {
@@ -45,6 +82,12 @@ $reports = $db->query('
 	table {
 		border-collapse:  collapse;
 		white-space: nowrap;
+	}
+	img.emoji {
+		height: 1em;
+		width: 1em;
+		margin: 0 .05em 0 .1em;
+		vertical-align: -0.1em;
 	}
 </style>
 </head>
@@ -130,7 +173,7 @@ while ($report = $reports->fetchArray()) {
 		echo '<td rowspan="' . $rowspan . '">' . h($report['policy_published_np']) . '</td>';
 		echo '<td rowspan="' . $rowspan . '">' . h($report['policy_published_fo']) . '</td>';	
 	}
-	echo '<td title="' . h($report['row_source_ip']) . '">' . isMySource($report['row_source_ip']) . h($report['row_souece_hostname']) . '</td>';
+	echo '<td title="' . h($report['row_source_ip'] . ' ' . $report['country']) . '">' . code2flag($report['country']) . isMySource($report['row_source_ip']) . h($report['row_souece_hostname']) . '</td>';
 	echo '<td>' . h($report['row_count']) . '</td>';
 	echo '<td>' . h($report['row_policy_evaluated_disposition']) . '</td>';
 	echo '<td>' . h($report['row_policy_evaluated_dkim']) . '</td>';
@@ -152,6 +195,10 @@ while ($report = $reports->fetchArray()) {
 }
 ?>
 </table>
+
+<script>
+twemoji.parse(document.body);
+</script>
 
 </body>
 </html>
